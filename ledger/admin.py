181506f47a17c224518aecf22578a271dd6bba23
CarylRae <caryl.chan@obf.ateneo.edu>
2024-03-15 14:46:11 +0800

    implemented log in flow

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from.models import Recipe, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Ingredient, Profile, Recipe, RecipeIngredient
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
 
 
 class RecipeAdmin(admin.ModelAdmin):
@@ -11,5 +18,18 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
 
-admin.site.register(Recipe, RecipeAdmin)
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
 
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [
+        ProfileInline,
+    ]
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
